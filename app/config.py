0aa4bf4e4094a6dabea1f000c8bbde54f237d951
Usmonbek Ravshanov <usmonbek_ravshanov@epam.com>
2024-01-02 15:57:36 +0500

    Till chapter 4

--- app/config.py
+++ app/config.py
@@ -1,5 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+env = os.getenv
+
+
 class Config(object):
-    pass
+    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{env('DB_USER')}:{env('DB_PASSWORD')}@{env('DB_HOST')}:{env('DB_PORT')}/{env('DB_NAME')}"
+    SECRET_KEY = env("SECRET_KEY")
 
 
 class ProdConfig(Config):
@@ -8,3 +16,4 @@ class ProdConfig(Config):
 
 class DevConfig(Config):
     DEBUG = True
+    PORT = env('APP_PORT')
